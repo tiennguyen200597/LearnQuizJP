4854a0c5cb032fc4603501d719c48a0e6c0fc46a
tiennv <tiennguyen200597@gmail.com>
2019-03-14 20:13:44 +0700

    Initial commit

---
+++ app/src/main/java/com/app/learnquizjp/activity/ChooseLvActivity.kt
@@ -0,0 +1,12 @@
+package com.app.learnquizjp.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseLvActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.app.learnquizjp.R.layout.activity_choose_lv)
+    }
+}
---
+++ app/src/main/java/com/app/learnquizjp/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.learnquizjp.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.app.learnquizjp.R.layout.activity_main)
+    }
+}
