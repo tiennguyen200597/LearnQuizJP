f44ba190d1eff7513a863a56120b27364d07e096
fox <namntph06792@fpt.edu.vn>
2019-03-22 15:33:30 +0700

    Add LearningActivity and layout

---
+++ app/src/main/java/com/app/learnquizjp/activity/LearningActivity.kt
@@ -0,0 +1,13 @@
+package com.app.learnquizjp.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.app.learnquizjp.R
+
+class LearningActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learning)
+    }
+}
