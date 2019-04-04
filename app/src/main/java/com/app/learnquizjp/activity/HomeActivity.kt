f9bb9ecf0e8f0160ba5e52df4fca6f19bd93642d
Fox <namntph06792@fpt.edu.vn>
2019-04-05 00:42:58 +0700

    Customize UI

--- app/src/main/java/com/app/learnquizjp/activity/HomeActivity.kt
+++ app/src/main/java/com/app/learnquizjp/activity/HomeActivity.kt
@@ -2,8 +2,8 @@ package com.app.learnquizjp.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 import android.support.v4.view.GravityCompat
@@ -19,6 +19,7 @@ import kotlinx.android.synthetic.main.app_bar_home.*
 
 
 class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     private val quizFragment: QuizFragment = QuizFragment()
     private val settingFragment : SettingFragment = SettingFragment()
 
@@ -31,24 +32,15 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar,
-            com.app.learnquizjp.R.string.navigation_drawer_open,
-            com.app.learnquizjp.R.string.navigation_drawer_close
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
         )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
-    }
-
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -61,19 +53,30 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            com.app.learnquizjp.R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            com.app.learnquizjp.R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
+            com.app.learnquizjp.R.id.nav_home -> {
+                startActivity(Intent(this, HomeActivity::class.java))
+                finish()
+            }
             com.app.learnquizjp.R.id.nav_learning -> {
                 // Handle the camera action
-                startActivity(Intent(this@HomeActivity,LearningActivity::class.java))
-                finish()
+                startActivity(Intent(this, LearningActivity::class.java))
             }
             com.app.learnquizjp.R.id.nav_quiz -> {
                 showFragmentQuiz()
@@ -88,7 +91,7 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
             }
             com.app.learnquizjp.R.id.nav_log_out -> {
-                startActivity(Intent(this@HomeActivity,LoginActivity::class.java))
+                startActivity(Intent(this, LoginActivity::class.java))
                 finish()
             }
         }
@@ -96,9 +99,9 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
-    fun showFragmentQuiz(){
-        var fragmentManager: FragmentManager=supportFragmentManager
-        var fragmentTransaction: FragmentTransaction=fragmentManager.beginTransaction()
+    private fun showFragmentQuiz(){
+        var fragmentManager: FragmentManager =supportFragmentManager
+        var fragmentTransaction: FragmentTransaction =fragmentManager.beginTransaction()
         if (quizFragment.isAdded){
             fragmentTransaction.show(quizFragment)
         }
@@ -118,5 +121,7 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
         fragmentTransaction.commit()
     }
+
+
 
 }
